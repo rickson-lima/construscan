9be12d93394e68f973f6facb7247593e9a5fc887
Rickson Lima <rickson.lima@sidia.com>
2023-12-19 15:02:06 -0400

    chore: setup firebase env vars

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -1,4 +1,17 @@
-declare module '@env' {
-  export const EMULATORS_HOST: string | undefined;
-  export const BYPASS_LOGIN: string | undefined;
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      EXPO_PUBLIC_FIREBASE_API_KEY: string;
+      EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN: string;
+      EXPO_PUBLIC_FIREBASE_PROJECT_ID: string;
+      EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET: string;
+      EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: string;
+      EXPO_PUBLIC_FIREBASE_APP_ID: string;
+      NODE_ENV: "development" | "production";
+    }
+  }
 }
+
+// If this file has no import/export statements (i.e. is a script)
+// convert it into a module by adding an empty export statement.
+export {};
